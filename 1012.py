c968918eb30cdcf41fbc4bacfc6de2f7b7b9436c
Percy Kim <106507373+userkim7@users.noreply.github.com>
2023-08-26 14:24:44 +0900

    Update 1012.py

--- 1012.py
+++ 1012.py
@@ -14,27 +14,28 @@
 ##입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다. 그 다음 줄부터 각각의 테스트 케이스에 대해 첫째 줄에는 배추를 심은 배추밭의 가로길이 M(1 ≤ M ≤ 50)과 세로길이 N(1 ≤ N ≤ 50), 그리고 배추가 심어져 있는 위치의 개수 K(1 ≤ K ≤ 2500)이 주어진다. 그 다음 K줄에는 배추의 위치 X(0 ≤ X ≤ M-1), Y(0 ≤ Y ≤ N-1)가 주어진다. 두 배추의 위치가 같은 경우는 없다.
 
 import sys
-sys.setrecursionlimit(100000)
-def surround(pos):
+sys.setrecursionlimit(100000) #재귀함수 제한 해제
+def surround(pos): #배추 유무|주변 살펴보기
     m,n=pos
-    if garden[n][m]==0:
-        return 0
-    garden[n][m]=0
-    for way in [[-1,0],[1,0],[0,-1],[0,1]]:
+    if garden[n][m]==0: 
+        return 0 #배추 없음->연결되지 않음->탐색 종료
+    garden[n][m]=0 #배추 있을 경우 중복 탐색 방지
+    for way in [[-1,0],[1,0],[0,-1],[0,1]]: #상하 좌우 탐색
         i,j=way
-        surround([m+i,n+j])
+        surround([m+i,n+j]) #배추 있을 경우 다시 상하좌우 탐색
 for i in range(int(sys.stdin.readline())):
     x,y,t=map(int,sys.stdin.readline().split())
-    garden=[[0 for k in range(x+2)] for j in range(y+2)]
-    for j in range(t):
+    garden=[[0 for k in range(x+2)] for j in range(y+2)] #정원 만들기->2칸 크게 만들어 탐색 미아 방지
+    for j in range(t): #정원에 배추 심기
         m,n=map(int,sys.stdin.readline().split())
         garden[n+1][m+1]=1
     num=0
     for n,row in enumerate(garden):
         for m,element in enumerate(row):
-            if element==1:
-                num+=1
+            if element==1: #배추 있을 시 연결된 배추 전부 뽑기
+                num+=1 #지렁이 수 추가
                 surround([m,n])
     print(num)
 
-###재귀함수
+##배추 연결시 그 주변 연결된 배추를 전부 뽑아 중복 탐색을 방지
+###재귀함수,좌표평면
