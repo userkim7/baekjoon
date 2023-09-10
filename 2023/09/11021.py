1d5886952736144b8694ecf33f1ab225b91ecc3e
Percy Kim <106507373+userkim7@users.noreply.github.com>
2023-09-10 13:21:09 +0900

    Update 11021.py

--- 2023/09/11021.py
+++ 2023/09/11021.py
@@ -1,7 +1,10 @@
 #11021
 
-i=1;exec(int(input())*'print(f"Case #{i}:",eval("+".join(input())));i+=1;')
+i=1
+exec(int(input())*'print(f"Case #{i}:",eval("+".join(input())));i+=1;')
 
 ###사칙연산
 
-####숏코딩 16위
+####숏코딩 6위
+i=1
+for a,c,b,c in[*open(0)][1:]:print(f'Case #{i}:',int(a)+int(b));i+=1
