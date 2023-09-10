631f0bbbbf7b125ac1561d304ae4b59076129f62
Percy Kim <106507373+userkim7@users.noreply.github.com>
2023-09-10 13:15:33 +0900

    Update 11022.py

--- 2023/09/11022.py
+++ 2023/09/11022.py
@@ -1,6 +1,10 @@
 i=1
-for a,c,b,c in[*open(0)][1:]:print(f'Case #{i}: {a} + {b} = {int(a)+int(b)}');i+=1
+for a,c,b,c in[*open(0)][1:]:
+  print(f'Case #{i}: {a} + {b} = {int(a)+int(b)}')
+  i+=1
 
 ###사칙연산
 
-####숏코딩 8위
+####숏코딩 4위
+i=1
+for a,c,b,c in[*open(0)][1:]:print(f'Case #{i}:',a,'+',b,'=',int(a)+int(b));i+=1
