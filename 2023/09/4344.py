9407772a78f3763972960f10bf2c686f03e47008
Percy Kim <106507373+userkim7@users.noreply.github.com>
2023-09-16 18:02:05 +0900

    Create 4344.py

---
+++ 2023/09/4344.py
@@ -0,0 +1,10 @@
+#4344
+
+for i in[*open(0)][1:]:
+  a,*b=map(int,i.split())
+  print(f'{sum(j*a>sum(b)for j in b)/a:.3%}')
+
+###평균
+
+####숏코딩 8위
+for i in[*open(0)][1:]:a,*b=map(int,i.split());print(f'{sum(j*a>sum(b)for j in b)/a:.3%}')
