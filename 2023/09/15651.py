e7e70a2de5a68ad8a3a6af3f9d352850e9b128f7
Percy Kim <106507373+userkim7@users.noreply.github.com>
2023-09-30 01:40:11 +0900

    Update 15651.py

--- 2023/09/15651.py
+++ 2023/09/15651.py
@@ -5,7 +5,7 @@ for i in range(a**b):
   print(c[(i//a**(b-j-1))%a],end=' ')
  print()
 
-###백트래킹
+###백트래킹,중복순열
 
 ####숏코딩 3위
 from itertools import*;a,_,b=input();*starmap(print,product(*[range(1,int(a)+1)]*int(b))),
