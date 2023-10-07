45ade95a5136470e2b11ff5503daf88b204f427a
Percy Kim <106507373+userkim7@users.noreply.github.com>
2023-10-07 10:40:03 +0900

    Update 2566.py

--- 2023/10/2566.py
+++ 2023/10/2566.py
@@ -2,4 +2,5 @@ m=[*map(int,open(0).read().split())];print(m[i:=m.index(max(m))],i//9+1,i%9+1)
 
 ###탐색
 
-####숏코딩 7위
+####숏코딩 2위
+m=open(0).read().split();print(m[i:=m.index(max(m,key=int))],i//9+1,i%9+1)
